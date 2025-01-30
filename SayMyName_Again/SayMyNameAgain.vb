052a6dacdef1915b8a02c88d6d41f29a067e60f0
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-30 08:56:16 -0700

    Write code to get a certian name input response

--- SayMyName_Again/SayMyNameAgain.vb
+++ SayMyName_Again/SayMyNameAgain.vb
@@ -12,7 +12,26 @@ Module SayMyNameAgain
     Sub Main()
         Dim YourName As String
 
+        Do
+            Console.Write("Please insert your name HERE:")
+            YourName = Console.ReadLine()
+            If YourName = "Emily" Or YourName = "Joe" Then
+                Console.WriteLine($"Hello, You must be {YourName}!")
+                Console.WriteLine()
+            ElseIf YourName = "Payden" Or YourName = "Tim" Then
+                Console.WriteLine($"OH MY, You are the one they speak of" &
+                                  $" the great and mighty {YourName}!")
+                Console.WriteLine()
+            ElseIf YourName IsNot "Emily" Or YourName IsNot "Joe" Then
+                Console.WriteLine()
+            ElseIf YourName IsNot "Payden" Or YourName IsNot "Tim" Then
+                Console.WriteLine()
+            End If
 
+            Console.WriteLine("If you would like to end" &
+            " type {Leave} in the name box.")
+            Console.WriteLine()
+        Loop Until YourName = "Leave"
     End Sub
 
 End Module
